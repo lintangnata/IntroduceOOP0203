d8b2b8013876c0e5617a493579b14ddb2935de9b
LintangNata <lintangnata4@gmail.com>
2025-04-28 14:57:16 +0700

    Membuat inputData

--- introduce.cpp
+++ introduce.cpp
@@ -3,7 +3,29 @@
 using namespace std;
 
 class Hewan {
+    private:
     string jenisKelamin;
+    public:
     string tipe;
     string umur;
+
+    void inputData() {
+        cout << "Masukkan Jenis Kelamin = " << endl;
+        cin >> jenisKelamin;
+        cout << "Masukkan Tipe Hewan = " << endl;
+        cin >> tipe;
+        cout << "Masukkan umur = " << endl;
+        cin >> umur;
+    }
+
+    void tapilHewan() {
+        
+    }
 };
+
+int main() {
+    Hewan ambon;
+    ambon.inputData();
+    cout << "Tipenya adalah = " << ambon.tipe << endl;
+    cout << "Umurnya adalah = " << ambon.umur << endl;
+}
